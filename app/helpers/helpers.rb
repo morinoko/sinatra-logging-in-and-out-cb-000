441c7c465a9beb558a35f381a63817f07a97fe76
Felice Forby <feliceforby@gmail.com>
2018-07-21 03:47:05 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,10 @@
 class Helpers
+  def self.current_user(session_hash)
+    id = session_hash[:user_id]
+    User.find(id)
+  end
+
+  def self.is_logged_in?(session_hash)
+    !!session_hash[:user_id]
+  end
 end
